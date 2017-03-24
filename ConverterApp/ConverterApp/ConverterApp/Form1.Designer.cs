6927e937f22c139fbe840de931080464e6eb2ab9
J377301 <j377301@tafe.wa.edu.au>
2017-03-24 14:36:32 +0800

    Add files via upload

---
+++ ConverterApp/ConverterApp/ConverterApp/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace ConverterApp
+{
+    partial class frm_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_UofM = new System.Windows.Forms.Label();
+            this.txt_UnitOfMeasure1 = new System.Windows.Forms.TextBox();
+            this.btn_CM_to_Inches = new System.Windows.Forms.Button();
+            this.btn_M_to_Feet = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.lbl_Display = new System.Windows.Forms.Label();
+            this.lbl_Convert = new System.Windows.Forms.Label();
+            this.txt_Convert1 = new System.Windows.Forms.TextBox();
+            this.txt_UnitOfMeasure2 = new System.Windows.Forms.TextBox();
+            this.txt_UnitOfMeasure3 = new System.Windows.Forms.TextBox();
+            this.txt_UnitOfMeasure4 = new System.Windows.Forms.TextBox();
+            this.txt_UnitOfMeasure5 = new System.Windows.Forms.TextBox();
+            this.txt_Convert2 = new System.Windows.Forms.TextBox();
+            this.txt_Convert3 = new System.Windows.Forms.TextBox();
+            this.txt_Convert4 = new System.Windows.Forms.TextBox();
+            this.txt_Convert5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_UofM
+            // 
+            this.lbl_UofM.AutoSize = true;
+            this.lbl_UofM.Location = new System.Drawing.Point(9, 20);
+            this.lbl_UofM.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_UofM.Name = "lbl_UofM";
+            this.lbl_UofM.Size = new System.Drawing.Size(82, 13);
+            this.lbl_UofM.TabIndex = 0;
+            this.lbl_UofM.Text = "Unit of Measure";
+            // 
+            // txt_UnitOfMeasure1
+            // 
+            this.txt_UnitOfMeasure1.Location = new System.Drawing.Point(106, 17);
+            this.txt_UnitOfMeasure1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_UnitOfMeasure1.Name = "txt_UnitOfMeasure1";
+            this.txt_UnitOfMeasure1.Size = new System.Drawing.Size(76, 20);
+            this.txt_UnitOfMeasure1.TabIndex = 1;
+            // 
+            // btn_CM_to_Inches
+            // 
+            this.btn_CM_to_Inches.Location = new System.Drawing.Point(261, 53);
+            this.btn_CM_to_Inches.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_CM_to_Inches.Name = "btn_CM_to_Inches";
+            this.btn_CM_to_Inches.Size = new System.Drawing.Size(122, 19);
+            this.btn_CM_to_Inches.TabIndex = 2;
+            this.btn_CM_to_Inches.Text = "Centimetres to Inches";
+            this.btn_CM_to_Inches.UseVisualStyleBackColor = true;
+            this.btn_CM_to_Inches.Click += new System.EventHandler(this.btn_CM_to_Inches_Click);
+            // 
+            // btn_M_to_Feet
+            // 
+            this.btn_M_to_Feet.Location = new System.Drawing.Point(261, 89);
+            this.btn_M_to_Feet.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_M_to_Feet.Name = "btn_M_to_Feet";
+            this.btn_M_to_Feet.Size = new System.Drawing.Size(122, 19);
+            this.btn_M_to_Feet.TabIndex = 3;
+            this.btn_M_to_Feet.Text = "Metres to Feet";
+            this.btn_M_to_Feet.UseVisualStyleBackColor = true;
+            this.btn_M_to_Feet.Click += new System.EventHandler(this.btn_M_to_Feet_Click);
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Location = new System.Drawing.Point(295, 181);
+            this.btn_Exit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(56, 19);
+            this.btn_Exit.TabIndex = 4;
+            this.btn_Exit.Text = "EXIT";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // lbl_Display
+            // 
+            this.lbl_Display.AutoSize = true;
+            this.lbl_Display.Location = new System.Drawing.Point(7, 129);
+            this.lbl_Display.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_Display.Name = "lbl_Display";
+            this.lbl_Display.Size = new System.Drawing.Size(0, 13);
+            this.lbl_Display.TabIndex = 5;
+            // 
+            // lbl_Convert
+            // 
+            this.lbl_Convert.AutoSize = true;
+            this.lbl_Convert.Location = new System.Drawing.Point(212, 129);
+            this.lbl_Convert.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_Convert.Name = "lbl_Convert";
+            this.lbl_Convert.Size = new System.Drawing.Size(0, 13);
+            this.lbl_Convert.TabIndex = 6;
+            // 
+            // txt_Convert1
+            // 
+            this.txt_Convert1.Location = new System.Drawing.Point(106, 129);
+            this.txt_Convert1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_Convert1.Name = "txt_Convert1";
+            this.txt_Convert1.Size = new System.Drawing.Size(76, 20);
+            this.txt_Convert1.TabIndex = 7;
+            // 
+            // txt_UnitOfMeasure2
+            // 
+            this.txt_UnitOfMeasure2.Location = new System.Drawing.Point(206, 17);
+            this.txt_UnitOfMeasure2.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_UnitOfMeasure2.Name = "txt_UnitOfMeasure2";
+            this.txt_UnitOfMeasure2.Size = new System.Drawing.Size(76, 20);
+            this.txt_UnitOfMeasure2.TabIndex = 8;
+            // 
+            // txt_UnitOfMeasure3
+            // 
+            this.txt_UnitOfMeasure3.Location = new System.Drawing.Point(307, 17);
+            this.txt_UnitOfMeasure3.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_UnitOfMeasure3.Name = "txt_UnitOfMeasure3";
+            this.txt_UnitOfMeasure3.Size = new System.Drawing.Size(76, 20);
+            this.txt_UnitOfMeasure3.TabIndex = 9;
+            // 
+            // txt_UnitOfMeasure4
+            // 
+            this.txt_UnitOfMeasure4.Location = new System.Drawing.Point(403, 17);
+            this.txt_UnitOfMeasure4.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_UnitOfMeasure4.Name = "txt_UnitOfMeasure4";
+            this.txt_UnitOfMeasure4.Size = new System.Drawing.Size(76, 20);
+            this.txt_UnitOfMeasure4.TabIndex = 10;
+            // 
+            // txt_UnitOfMeasure5
+            // 
+            this.txt_UnitOfMeasure5.Location = new System.Drawing.Point(496, 17);
+            this.txt_UnitOfMeasure5.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_UnitOfMeasure5.Name = "txt_UnitOfMeasure5";
+            this.txt_UnitOfMeasure5.Size = new System.Drawing.Size(76, 20);
+            this.txt_UnitOfMeasure5.TabIndex = 11;
+            // 
+            // txt_Convert2
+            // 
+            this.txt_Convert2.Location = new System.Drawing.Point(206, 129);
+            this.txt_Convert2.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_Convert2.Name = "txt_Convert2";
+            this.txt_Convert2.Size = new System.Drawing.Size(76, 20);
+            this.txt_Convert2.TabIndex = 12;
+            // 
+            // txt_Convert3
+            // 
+            this.txt_Convert3.Location = new System.Drawing.Point(307, 129);
+            this.txt_Convert3.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_Convert3.Name = "txt_Convert3";
+            this.txt_Convert3.Size = new System.Drawing.Size(76, 20);
+            this.txt_Convert3.TabIndex = 13;
+            // 
+            // txt_Convert4
+            // 
+            this.txt_Convert4.Location = new System.Drawing.Point(403, 129);
+            this.txt_Convert4.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_Convert4.Name = "txt_Convert4";
+            this.txt_Convert4.Size = new System.Drawing.Size(76, 20);
+            this.txt_Convert4.TabIndex = 14;
+            // 
+            // txt_Convert5
+            // 
+            this.txt_Convert5.Location = new System.Drawing.Point(496, 129);
+            this.txt_Convert5.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_Convert5.Name = "txt_Convert5";
+            this.txt_Convert5.Size = new System.Drawing.Size(76, 20);
+            this.txt_Convert5.TabIndex = 15;
+            // 
+            // frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 460);
+            this.Controls.Add(this.txt_Convert5);
+            this.Controls.Add(this.txt_Convert4);
+            this.Controls.Add(this.txt_Convert3);
+            this.Controls.Add(this.txt_Convert2);
+            this.Controls.Add(this.txt_UnitOfMeasure5);
+            this.Controls.Add(this.txt_UnitOfMeasure4);
+            this.Controls.Add(this.txt_UnitOfMeasure3);
+            this.Controls.Add(this.txt_UnitOfMeasure2);
+            this.Controls.Add(this.txt_Convert1);
+            this.Controls.Add(this.lbl_Convert);
+            this.Controls.Add(this.lbl_Display);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_M_to_Feet);
+            this.Controls.Add(this.btn_CM_to_Inches);
+            this.Controls.Add(this.txt_UnitOfMeasure1);
+            this.Controls.Add(this.lbl_UofM);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frm_Main";
+            this.Text = "ATCA Gas Converter";
+           
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_UofM;
+        private System.Windows.Forms.TextBox txt_UnitOfMeasure1;
+        private System.Windows.Forms.Button btn_CM_to_Inches;
+        private System.Windows.Forms.Button btn_M_to_Feet;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Label lbl_Display;
+        private System.Windows.Forms.Label lbl_Convert;
+        private System.Windows.Forms.TextBox txt_Convert1;
+        private System.Windows.Forms.TextBox txt_UnitOfMeasure2;
+        private System.Windows.Forms.TextBox txt_UnitOfMeasure3;
+        private System.Windows.Forms.TextBox txt_UnitOfMeasure4;
+        private System.Windows.Forms.TextBox txt_UnitOfMeasure5;
+        private System.Windows.Forms.TextBox txt_Convert2;
+        private System.Windows.Forms.TextBox txt_Convert3;
+        private System.Windows.Forms.TextBox txt_Convert4;
+        private System.Windows.Forms.TextBox txt_Convert5;
+    }
+}
+
---
+++ ConverterApp/ConverterApp/ConverterApp/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConverterApp
+    // This program was written by Gail Mosdell
+    // It forms the base of a converter program for the OS-Assessment Two for Cert IV
+    // Date : February 2017
+{
+    public partial class frm_Main : Form
+    {
+        public frm_Main()
+        {
+            InitializeComponent();
+        }
+
+        // Global Variables and Constants
+        double[] dbl_UofM;
+        double[] dbl_Convert;
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_CM_to_Inches_Click(object sender, EventArgs e)
+        {
+            const double CM_TO_INCH = 0.3937;
+            dbl_UofM = new double[5];
+            
+
+            dbl_Convert = new double[5];
+            // validate user entry and convert to a double
+            
+            //First Value
+
+            if (!double.TryParse(txt_UnitOfMeasure1.Text, out dbl_UofM[0] ))
+            {
+                MessageBox.Show("A numeric must be entered. Please re-enter the value.");
+                txt_UnitOfMeasure1.Clear();
+                txt_UnitOfMeasure1.Focus();
+                txt_Convert1.Clear();
+                lbl_Convert.Text = "";
+                lbl_Display.Text = "";
+            }
+            else
+            {
+                dbl_Convert[0] = dbl_UofM[0] * CM_TO_INCH;
+                txt_Convert1.Text = dbl_Convert[0].ToString();
+                lbl_Display.Text = txt_UnitOfMeasure1.Text + " centimetres is converted to ";
+                lbl_Convert.Text = " inches.";
+            }
+
+            //Second Vale
+
+            if (!double.TryParse(txt_UnitOfMeasure2.Text, out dbl_UofM[1]))
+            {
+                MessageBox.Show("A numeric must be entered. Please re-enter the value.");
+                txt_UnitOfMeasure1.Clear();
+                txt_UnitOfMeasure1.Focus();
+                txt_Convert2.Clear();
+                lbl_Convert.Text = "";
+                lbl_Display.Text = "";
+            }
+            else
+            {
+                dbl_Convert[1] = dbl_UofM[1] * CM_TO_INCH;
+                txt_Convert2.Text = dbl_Convert[1].ToString();
+                lbl_Display.Text = txt_UnitOfMeasure2.Text + " centimetres is converted to ";
+                lbl_Convert.Text = " inches.";
+            }
+
+            //Third Value
+
+            if (!double.TryParse(txt_UnitOfMeasure3.Text, out dbl_UofM[2]))
+            {
+                MessageBox.Show("A numeric must be entered. Please re-enter the value.");
+                txt_UnitOfMeasure1.Clear();
+                txt_UnitOfMeasure1.Focus();
+                txt_Convert3.Clear();
+                lbl_Convert.Text = "";
+                lbl_Display.Text = "";
+            }
+            else
+            {
+                dbl_Convert[2] = dbl_UofM[2] * CM_TO_INCH;
+                txt_Convert3.Text = dbl_Convert[2].ToString();
+                lbl_Display.Text = txt_UnitOfMeasure3.Text + " centimetres is converted to ";
+                lbl_Convert.Text = " inches.";
+            }
+
+            //Fourth Value
+
+            if (!double.TryParse(txt_UnitOfMeasure4.Text, out dbl_UofM[3]))
+            {
+                MessageBox.Show("A numeric must be entered. Please re-enter the value.");
+                txt_UnitOfMeasure1.Clear();
+                txt_UnitOfMeasure1.Focus();
+                txt_Convert4.Clear();
+                lbl_Convert.Text = "";
+                lbl_Display.Text = "";
+            }
+            else
+            {
+                dbl_Convert[3] = dbl_UofM[3] * CM_TO_INCH;
+                txt_Convert4.Text = dbl_Convert[3].ToString();
+                lbl_Display.Text = txt_UnitOfMeasure4.Text + " centimetres is converted to ";
+                lbl_Convert.Text = " inches.";
+            }
+
+            //Fifth Value
+
+            if (!double.TryParse(txt_UnitOfMeasure5.Text, out dbl_UofM[4]))
+            {
+                MessageBox.Show("A numeric must be entered. Please re-enter the value.");
+                txt_UnitOfMeasure1.Clear();
+                txt_UnitOfMeasure1.Focus();
+                txt_Convert5.Clear();
+                lbl_Convert.Text = "";
+                lbl_Display.Text = "";
+            }
+            else
+            {
+                dbl_Convert[4] = dbl_UofM[4] * CM_TO_INCH;
+                txt_Convert5.Text = dbl_Convert[4].ToString();
+                lbl_Display.Text = txt_UnitOfMeasure5.Text + " centimetres is converted to ";
+                lbl_Convert.Text = " inches.";
+            }
+        }
+
+        private void btn_M_to_Feet_Click(object sender, EventArgs e)
+        {
+            // Create Code for this procedure
+        }
+
+        
+    }
+}
